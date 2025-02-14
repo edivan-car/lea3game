d7dc6d6ab70dc6b2e8302c456ea4bac34543e32b
edivan-car <edivam.c@gmail.com>
2025-02-14 13:19:14 -0300

    Project created

---
+++ backend/src/main/java/br/com/lea3/lea3game/Lea3gameApplication.java
@@ -0,0 +1,13 @@
+package br.com.lea3.lea3game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lea3gameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lea3gameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/lea3/lea3game/Lea3gameApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lea3.lea3game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lea3gameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
